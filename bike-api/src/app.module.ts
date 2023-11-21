d819df3dbdaca661c1015c63c509e9e6ca7a8b1b
Marilynmonroy <artlyn.monroy@gmail.com>
2023-11-21 10:06:52 -0300

    api

--- bike-api/src/app.module.ts
+++ bike-api/src/app.module.ts
@@ -5,7 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
 import { BicyclesModule } from './bicycles/bicycles.module';
-import { EmailServerModule } from './email-server/email-server.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
@@ -15,7 +14,6 @@ import { UserModule } from './user/user.module';
     CustomersModule,
     OrdersModule,
     BicyclesModule,
-    EmailServerModule,
     AuthModule,
     UserModule,
   ],
